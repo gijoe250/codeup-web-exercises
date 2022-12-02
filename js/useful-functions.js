092dcda51b978351125ff286bd34326bd5ffd59a
Joe Garcia <gijoe250@gmail.com>
2022-12-02 16:29:14 -0600

    adding more array exercises and adding isVowel function to useful functions

--- js/useful-functions.js
+++ js/useful-functions.js
@@ -12,3 +12,15 @@ function isEven(num){
 function randomNumber(min, max) {
     return (Math.floor(Math.random() * (max+1 - min)) + min);
 }
+
+function isVowel(vowel){
+    if (typeof vowel == "string") {
+        let vowelLower = vowel.toLowerCase();
+        if (vowelLower === "a" || vowelLower === "e" || vowelLower === "i" || vowelLower === "o" || vowelLower === "u") {
+            return true;
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
