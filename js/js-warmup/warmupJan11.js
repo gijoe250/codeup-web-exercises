0e7b0538e2a40b3ddc2a00340760567dca316e19
Joe Garcia <gijoe250@gmail.com>
2023-01-11 09:10:12 -0600

    adding warmup for Jan11, deleting warmup html since unecessary, and updating dom query with button events

---
+++ js/js-warmup/warmupJan11.js
@@ -0,0 +1,9 @@
+// This loop should print the numbers from 0 through 9 in the console
+
+for (let i = 0; i < 10; i++) {
+    console.log("The value of i is " + i);
+    //remove the i++
+    // i++;
+}
+
+// desired output : 0, 1 ,2,  3, 4, 5, 6, 7, 8, 9
