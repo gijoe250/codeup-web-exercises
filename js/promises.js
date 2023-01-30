6be1357f59cb4479db07497dfb3e1b9862ffd7e6
Joe Garcia <gijoe250@gmail.com>
2023-01-30 14:27:50 -0600

    adding promises exercise

---
+++ js/promises.js
@@ -0,0 +1,24 @@
+( () => {
+    "use strict"
+
+    // TODO: Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
+    let userInfo;
+
+    $('#searchButton').click(async function(e) {
+        e.preventDefault();
+        userInfo = await getUserName($('#inputBox').val());
+        console.log(userInfo[0].created_at);
+        console.log(userInfo[0].actor.login);
+    });
+
+    async function getUserName(userName){
+        try{
+            let response = await fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': `token ${keys.gitHub}`}});
+            let data = await response.json()
+            return data;
+        }
+        catch (error){
+            return error;
+        }
+    }
+})();
