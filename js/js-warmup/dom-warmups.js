2e665d84a028fb307f18e1e4b21d0196dfc5ac31
Joe Garcia <gijoe250@gmail.com>
2023-01-30 09:27:32 -0600

    adding dom exercise

---
+++ js/js-warmup/dom-warmups.js
@@ -0,0 +1,9 @@
+(function (){
+    "use strict";
+
+    // Add a button that when clicked, changes the background color to red
+    $('button').click(function() {
+        $(this).css('background-color', 'red');
+    });
+
+})();
