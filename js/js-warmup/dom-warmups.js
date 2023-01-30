a18b7ab38ba4b49c3b7a7ff88ebff41787ed5fb2
Joe Garcia <gijoe250@gmail.com>
2023-01-30 09:37:25 -0600

    adding on usage for DOM ex

--- js/js-warmup/dom-warmups.js
+++ js/js-warmup/dom-warmups.js
@@ -3,7 +3,13 @@
 
     // Add a button that when clicked, changes the background color to red
     $('button').click(function() {
-        $(this).css('background-color', 'red');
+        $('body').css('background-color', 'red');
     });
+    /**
+     * additional example using the .on
+     */
+    // $('button').on('click', (e) => {
+    //     $('body').css('background-color', 'red');
+    // });
 
 })();
