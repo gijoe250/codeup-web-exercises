190972945fef38bd5a7c3f2d36ca8ede66749354
Joe Garcia <gijoe250@gmail.com>
2023-01-30 16:21:37 -0600

    adding bonus 1 to promises ex

--- js/promises.js
+++ js/promises.js
@@ -4,12 +4,27 @@
     // TODO: Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
     let userInfo;
 
-    $('#searchButton').click(async function(e) {
+    $('#searchButton1').click(async function(e) {
         e.preventDefault();
-        userInfo = await getUserName($('#inputBox').val());
+        userInfo = await getUserName($('#inputBox1').val());
         const date = new Date(userInfo[0].created_at);
         console.log(date);
         console.log(userInfo[0].actor.login);
+    });
+
+    $('#searchButton2').on('click', async (e) => {
+       try {
+           e.preventDefault();
+           let response = await fetch('./../data/profiles.json');
+           let data = await response.json();
+           let input = $('#inputBox2').val();
+           $('body').css('background-color', `${data[input]}`);
+           $("#inputBox2").val("");
+       }
+       catch (error){
+           console.log("it failed");
+           return error;
+       }
     });
 
     async function getUserName(userName){
@@ -22,4 +37,7 @@
             return error;
         }
     }
+
+
+
 })();
